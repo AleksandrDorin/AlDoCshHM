08c85bca949a5744e986c45765ec307914790fe1
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-06 20:19:43 +0300

    Task10 Completed

---
+++ Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+int res = 0;
+
+
+if (a % b == 0)
+{
+    Console.WriteLine("Число кратное");
+}
+
+else
+
+{
+    res = a % b;
+    Console.WriteLine("Число не кратное, остаток: " + res);
+}
