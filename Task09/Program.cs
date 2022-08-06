e487ac3693a83956b5a695ca2ee834349c7ac785
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-06 20:17:07 +0300

    Task09 Completed

---
+++ Task09/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите любое трехзначное число");
+int a = int.Parse(Console.ReadLine());
+
+int b = int.Parse(a.ToString().Remove(1, 1));
+Console.WriteLine("Мы удалили вторую цифру введенного трехзначного числа ");
+Console.WriteLine("Получилось число: " + b);
