99408c16ac0d29170bf7cd1349d514d627a26646
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-06 20:22:25 +0300

    Task11 Completed

---
+++ Task11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите любое трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+
+string b = a.ToString();
+
+if (a / 100 > 0)
+{
+    Console.WriteLine("Третья цифра числа " + b + " является: " + b[2]);
+}
+else
+{
+    Console.WriteLine("У числа " + b + " нет третьей цифры");
+}
