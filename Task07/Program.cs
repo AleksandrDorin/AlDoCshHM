27186ccb2407498e0050543a9dd97a4e376b5328
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-06 20:09:25 +0300

    Task07 Completed

---
+++ Task07/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите любое трехзначное число: ");
+string a = Console.ReadLine();
+
+Console.WriteLine("Вторая цифра числа " + a + ": "  + a[1]);
