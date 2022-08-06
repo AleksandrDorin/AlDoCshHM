3f04f94623ff1eb45317fca2023a475463b1db8b
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-06 20:03:49 +0300

    Task04 Completed

---
+++ Task04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Введенное число является четным !");
+}
+
+else
+{
+    Console.WriteLine("Введенное число является нечетным !");
+}
