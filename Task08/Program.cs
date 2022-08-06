416c7ef21439a5602b6d07b0ad4f4ea08216bf21
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-06 20:12:49 +0300

    Task08 Completed

---
+++ Task08/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int a = new Random().Next(10, 99);
+
+Console.WriteLine("Случайное число из отрезка [10, 99] " + a);
+
+string str = a.ToString(); 
+
+int numA = int.Parse(str[0].ToString());
+int numB = int.Parse(str[1].ToString());
+int max = int.Parse(str[0].ToString());
+
+if (numA > numB)
+{
+    max = numA;
+}
+else
+{
+    max = numB;
+}
+
+Console.WriteLine("Наибольшая цифра = " + max);
