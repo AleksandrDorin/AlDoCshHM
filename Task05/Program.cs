660baab2d46c523f1f3ab92a961c27ee26dbbde6
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-06 20:06:38 +0300

    Task05 Completed

---
+++ Task05/Program.cs
@@ -0,0 +1,11 @@
+﻿//Показать числа от -N до N
+Console.WriteLine("Введите -N число");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите N число");
+int b = int.Parse(Console.ReadLine());
+
+while (a < b)
+{
+    Console.WriteLine(a++);
+}
